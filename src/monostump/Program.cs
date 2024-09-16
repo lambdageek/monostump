df3411a4fe179167f29d0500600106457b23b41c
Aleksey Kliger <alklig@microsoft.com>
2024-09-16 11:48:41 -0400

    use structure logger's node analyzer

--- src/monostump/Program.cs
+++ src/monostump/Program.cs
@@ -1,8 +1,14 @@
 ﻿using Microsoft.Build.Logging.StructuredLogger;
 
 using var stream = File.OpenRead(args[0]);
-var records = BinaryLog.ReadRecords(stream);
-foreach (var record in records)
+var build = BinaryLog.ReadBuild(args[0]);
+BuildAnalyzer.AnalyzeBuild(build);
+var root = build.GetRoot();
+if (root is TreeNode node)
 {
-    Console.WriteLine(record);
+    Console.WriteLine(node.ToString());
+    foreach (var child in node.Children)
+    {
+        Console.WriteLine(child.ToString());
+    }
 }
