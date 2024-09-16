ebc4eba13fe229556de5a4b9386bb6070eee874c
Aleksey Kliger <alklig@microsoft.com>
2024-09-16 11:17:28 -0400

    scafffolding

---
+++ src/monostump/Program.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Build.Logging.StructuredLogger;
+
+using var stream = File.OpenRead(args[0]);
+var records = BinaryLog.ReadRecords(stream);
+foreach (var record in records)
+{
+    Console.WriteLine(record);
+}
